11a59cf0453ed31bef6d1ae8ac65af92aac7b085
yugoru <yulia.gorshkova@soft-machine.ru>
2021-03-17 11:18:07 +0300

    Самый безгеморройный вариант

---
+++ src/test/java/tests/TextBoxTestsYugoru.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTestsYugoru {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void successfulTest() {
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue("Iuliia");
+        $("#lastName").setValue("Gorshkova");
+        $("#userEmail").setValue("iuliia@qahacking.ru");
+        $("label[for=gender-radio-2]").click();
+        $("#userNumber").setValue("9996387733");
+        $("#dateOfBirthInput").setValue("09 Mar 2006");
+        $("#subjectsInput").setValue("Sub");
+        $("#hobbiesWrapper").find(byText("Music")).click();
+        $("#hobbiesWrapper").find(byText("Reading")).click();
+        $("#uploadPicture").sendKeys("src/files/1.jpeg");
+        $("#currentAddress").setValue("St.Petersburg, Viborgskaya naberejnaya");
+        $("#stateCity-wrapper").find(byId("state")).click();
+        $("#react-select-3-option-2").click();
+        $("#stateCity-wrapper").find(byId("city")).click();
+        $("#react-select-4-option-1").click();
+        $("#submit").click();
+        $(".modal-content").find(byCssSelector("#closeLargeModal")).click();
+
+    }
+}
