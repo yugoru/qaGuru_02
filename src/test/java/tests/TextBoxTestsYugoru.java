e2c7f4321433e9ce475d9d9cf5dcc36288de6727
yugoru <yulia.gorshkova@soft-machine.ru>
2021-04-04 15:00:59 +0300

    Окончательный вариант (просто больше нет сил)

--- src/test/java/tests/TextBoxTestsYugoru.java
+++ src/test/java/tests/TextBoxTestsYugoru.java
@@ -14,7 +14,8 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTestsYugoru {
-    String firstName = "Ada", lastName = "Lovelace",
+    String firstName = "Ada",
+            lastName = "Lovelace",
             subject = "Hindi",
             subject2 = "Maths",
             currentAddress = "Russia \n St.Petersburg \n Viborgskaya naberejnaya \n Rozoviy dom",
@@ -22,18 +23,18 @@ public class TextBoxTestsYugoru {
             phone = "0909090909",
             gender = "label[for=gender-radio-2]",
             genderName = "Female",
-            hobby2 = "Music", hobby3 = "Reading",
+            hobby2 = "Music",
+            hobby3 = "Reading",
             month = "April",
             year = "1991",
             day = ".react-datepicker__day--026",
-            convertedDate = "26 "+ month + "," + year,
+            convertedDate = "26 " + month + "," + year,
             state = "Haryana",
+            fileName = "1.jpeg",
             city = "Panipat";
 
-
-    Path fileP = Paths.get("./qaGuru_2/src/files/1.jpeg");
-    File file = new File(fileP.toString());
-    String fileName = fileP.getFileName().toString();
+    Path filePath = Paths.get("src/test/resources/" + fileName);
+    File file = new File(filePath.toString());
 
     @BeforeAll
     static void setup() {
@@ -53,16 +54,13 @@ public class TextBoxTestsYugoru {
         $(".react-datepicker__month-select").find(byText(month)).click();
         $(".react-datepicker__year-select").find(byText(year)).click();
         $(day).click();
-        $("#subjectsInput").sendKeys(subject);
-        $(".css-26l3qy-menu div").find(byText(subject)).click();
-        $("#subjectsInput").sendKeys(subject2);
-        $(".css-26l3qy-menu div").find(byText(subject2)).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#subjectsInput").setValue(subject2).pressEnter();
         $("#hobbiesWrapper").find(byText(hobby2)).click();
         $("#hobbiesWrapper").find(byText(hobby3)).click();
         $("#uploadPicture").uploadFile(file);
         $("#currentAddress").setValue(currentAddress);
-        $("#stateCity-wrapper").scrollIntoView(true);
-        $("#stateCity-wrapper").find(byId("state")).click();
+        $("#stateCity-wrapper").$("#state").scrollTo().click();
         $(".css-26l3qy-menu div").find(byText(state)).click();
         $("#stateCity-wrapper").find(byId("city")).click();
         $(".css-26l3qy-menu div").find(byText(city)).click();
@@ -80,8 +78,10 @@ public class TextBoxTestsYugoru {
                 text(fileName),
                 text(currentAddress),
                 text(state + " " + city));
+    }
 
+    @Test
+    void closeFormTest() {
         $(".modal-content").find(byCssSelector("#closeLargeModal")).click();
-
     }
 }
